99b09c30f89a3f52053f2daf52cafe9da62b60c5
Latipov <s.l.t1998@mail.ru>
2023-05-18 23:58:48 +0300

    Реализовал запись объекта на фронте полученного с бэкенд

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
 const link = 'http://api.weatherstack.com/current?access_key=dd61d8fd4a8c6f57f599045b595b9961';
 
-const store = {
+let store = {
    city: 'London',
-   feelslike: 0, 
-   cloudcover: 0, 
+   feelslike: 0,
+   cloudcover: 0,
    tempature: 0,
    humidity: 0,
    observationTime: '00:00 AM',
+   pressure: 0,
+   uvIndex: 0,
+   visibility: 0,
+   isDay: 'yes',
+   decription: '',
+   windSpeed: 0,
+
+
 };
 
 const fetchData = async () => {
    const result = await fetch(`${link}&query=${store.city}`);
    const data = await result.json();
 
-   const 
-      { current: { feelslike, cloudcover, tempature },
-      location: { name }
-   } = data
-   
+   const
+      { current:
+         {
+            feelslike,
+            cloudcover,
+            tempature,
+            observation_time: observationTime,
+            pressure,
+            uvIndex: uv_index,
+            visibility,
+            is_day: isDay,
+            weather_decriptions: decription,
+            wind_speed: windSpeed,
+
+
+
+         },
+         location: { name }
+      } = data
+
    console.log('data', data)
+}
+
+store = {
+   ...store,
+   isDay,
+   uvIndex,
+   pressure,
+   humidity,
+   tempature,
+   feelslike,
+   windSpeed,
+   cloudcover,
+   visibility,
+   observationTime,
+   decription: decription[0],
 }
 
 fetchData();
